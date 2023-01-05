50848a90c74c315860287d1a312de889c8c922df
Vera Kuznetsova <kuznetsova-vera@yandex.ru>
2023-01-05 20:47:49 +0200

    CW # 30

---
+++ app.js
@@ -0,0 +1,10 @@
+"use strict";
+var _loop_1 = function (i) {
+    setTimeout(function () { return console.log(i); });
+};
+for (var i = 0; i < 3; i++) {
+    _loop_1(i);
+}
+//let a = 10;
+//console.log(`a = ${a}`);
+//# sourceMappingURL=app.js.map
---
+++ app.ts
@@ -0,0 +1,24 @@
+/*
+for (let i = 0; i < 3; i++) {
+    setTimeout(()=> console.log(i));
+}
+let a = 10;
+console.log(`a = ${a}`);
+*/
+
+// variables
+let hello = 'world';
+let b;
+b =10;
+b= 'abc';
+let c: string;
+// function
+function getRandomNumber (min:number, max:number):number {
+return Math.trunc (min + Math.random() * (max - min +1));
+}
+
+function shiftCipher(str: string, shift: number = 1): string {
+    return '';
+}
+
+
